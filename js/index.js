7987e796f98b6b2e330d1c866e89bbe59d6f527a
imrancoder10 <imranhossain1148@gmail.com>
2024-09-20 15:36:03 +0600

    get phone number and pin number with condition check(if---else)

--- js/index.js
+++ js/index.js
@@ -5,5 +5,14 @@
 document.getElementById('login-btn').addEventListener('click', function (event) {
     // step - 2: prevent default behavior(prevent reloading browser)
     event.preventDefault();
-   
+    // step - 3: get the phone number and pin number
+    const inputPhoneNumber = document.getElementById('input-phone-number').value;
+    const inputPinNumber = document.getElementById('input-pin-number').value;
+
+    if(inputPhoneNumber==='5' && inputPinNumber==='1234'){
+        console.log('You are logged in.');
+    }
+    else{
+        alert('Wrong phone number or pin');
+    }
 });
